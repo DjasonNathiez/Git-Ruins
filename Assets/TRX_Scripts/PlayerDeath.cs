33ba92adab1e13b7ace88fbd330bff7479e650fd
Djason <nathiez.djason@outlook.fr>
2020-05-11 10:47:52 +0200

    2.2.6a
    -commit base.
    -gitignore.

---
+++ Assets/TRX_Scripts/PlayerDeath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private GameObject respawnPoint;
+    private GameObject currentPlayer;
+
+
+
+    void Start()
+    {
+        respawnPoint = GameObject.FindWithTag("Respawn");
+        currentPlayer = GameObject.Find("Character");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Death()
+    {
+        //play Death animation
+
+        //Teleport the player GameObject to the respawn point location
+        currentPlayer = GameObject.FindWithTag("Player");
+        currentPlayer.transform.position = respawnPoint.transform.position;
+        
+    }
+
+}
