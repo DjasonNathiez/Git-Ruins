b8ec2af63355e3bb2b7b462ec0441b86e3fe9c81
Djason <nathiez.djason@outlook.fr>
2020-05-12 18:40:40 +0200

    2.3.1
    Debug :
    -Camera clamp.
    
    Add :
    -Camera switch.

---
+++ Assets/DN_Scripts/Camera/CameraSwitch.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    private GameObject cameraLow;
+    private GameObject cameraLarge;
+    private GameObject cameraMain;
+
+    public bool CameraLow;
+    public bool CameraLarge;
+    public bool CameraMain;
+
+    private void Awake()
+    {
+        cameraLow = GameObject.Find("Camera_Low");
+        cameraLarge = GameObject.Find("Camera_Large");
+        cameraMain = GameObject.Find("Main Camera");
+    }
+
+    private void Start()
+    {
+        if(CameraMain == true)
+        {
+            cameraMain.SetActive(true);
+            cameraLow.SetActive(false);
+            cameraLarge.SetActive(false);
+        }
+
+        if (CameraLow == true)
+        {
+            cameraMain.SetActive(false);
+            cameraLow.SetActive(true);
+            cameraLarge.SetActive(false);
+        }
+
+        if (CameraLarge == true)
+        {
+            cameraMain.SetActive(false);
+            cameraLow.SetActive(false);
+            cameraLarge.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CameraLow"))
+        {
+            Debug.Log("camlow find");
+            cameraMain.SetActive(false);
+            cameraLow.SetActive(true);
+            cameraLarge.SetActive(false);
+        }
+
+        if (collision.CompareTag("CameraLarge"))
+        {
+            Debug.Log("camlarge find");
+            cameraMain.SetActive(false);
+            cameraLow.SetActive(false);
+            cameraLarge.SetActive(true);
+        }
+
+        if (collision.CompareTag("MainCamera"))
+        {
+            Debug.Log("cammain find");
+            cameraMain.SetActive(true);
+            cameraLow.SetActive(false);
+            cameraLarge.SetActive(false);
+        }
+    }
+}
