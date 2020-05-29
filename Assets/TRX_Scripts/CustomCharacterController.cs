ddc9af6bef9c4dd397878272f25e6204e9a5bf05
Toinon Robaux <toinon.robaux@lilo.org>
2020-05-29 22:38:01 +0200

    2.4.6.4
    - Anim walljump

--- Assets/TRX_Scripts/CustomCharacterController.cs
+++ Assets/TRX_Scripts/CustomCharacterController.cs
@@ -461,15 +461,18 @@ public class CustomCharacterController : MonoBehaviour
     void GroundingUpdate()
     {
         //Setup des raycast pour mettre à jour le grounding
-        int numHits = playerCollider.Raycast(-Vector2.up, rGroundCast, 1.3f, groundLayer);
-        int wallHitsLeft = playerCollider.Raycast(Vector2.left, rWallCast, 1.4f, wallJumpLayer);
-        int wallHitsRight = playerCollider.Raycast(-Vector2.left, rWallCast, 1.4f, wallJumpLayer);
+        int numHits = playerCollider.Raycast(-Vector2.up, rGroundCast, 1.3f, (groundLayer | wallJumpLayer));
+        int wallHitsLeft = playerCollider.Raycast(Vector2.left, rWallCast, 0.8f, wallJumpLayer);
+        int wallHitsRight = playerCollider.Raycast(-Vector2.left, rWallCast, 0.8f, wallJumpLayer);
 
         //Grounding au sol
         if (numHits > 0)
         {
             groundType = GroundType.ground;
             canJump = true;
+            animator.SetBool("isGrinding", false);
+            Debug.Log(groundType);
+
         }
         //Grounding sur un mur de WallJump
         else if (wallHitsLeft > 0)
@@ -478,6 +481,7 @@ public class CustomCharacterController : MonoBehaviour
             groundType = GroundType.wallJump;
             isLeftWallJump = true;
             canJump = true;
+            animator.SetBool("isGrinding", true);
         }
         else if (wallHitsRight > 0)
         {
@@ -485,12 +489,16 @@ public class CustomCharacterController : MonoBehaviour
             groundType = GroundType.wallJump;
             isLeftWallJump = false;
             canJump = true;
+            animator.SetBool("isGrinding", true);
+
         }
         //Pas de Grounding (vide)
         else
         {
             groundType = GroundType.none;
             canJump = false;
+            animator.SetBool("isGrinding", false);
+
         }
     }
 
