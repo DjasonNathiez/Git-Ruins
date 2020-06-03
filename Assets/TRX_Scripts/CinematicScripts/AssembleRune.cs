9cf29c7eae0085f708cab3a0446cab3b128b00a3
Toinon Robaux <toinon.robaux@lilo.org>
2020-06-03 02:48:14 +0200

    2.8.1 (BUILD SAFE 2.7.7)
    - Intégration de la cinématique de fin (début)

---
+++ Assets/TRX_Scripts/CinematicScripts/AssembleRune.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AssembleRune : MonoBehaviour
+{
+    private BoxCollider2D triggerCollider;
+    private Collider2D playerCollider;
+    private Rigidbody2D playerRigidbody;
+
+    private Animator runeUniteAnimator;
+    private Animator playerAnimator;
+    public GameObject runeParticle;
+
+    public GameObject templeFX1;
+    public GameObject templeFX2;
+    public GameObject templeFX3;
+    public GameObject templeFX4;
+    public GameObject templeFX5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggerCollider = gameObject.GetComponent<BoxCollider2D>();
+        playerCollider = GameObject.FindWithTag("Player").GetComponent<Collider2D>();
+        playerRigidbody = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+        runeUniteAnimator = GameObject.Find("RunesUnite").GetComponent<Animator>();
+        playerAnimator = GameObject.FindWithTag("Player").GetComponent<Animator>();
+
+        //playerAnimator.SetBool("Apparition", true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D playerCollider)
+    {
+        StartCoroutine("Timeline");
+        Debug.Log("Timeline lancée !");
+    }
+
+
+
+    IEnumerator Timeline()
+    {
+        //Téléportation dans la scène
+        yield return new WaitForSeconds(2.5f);
+
+        //Apparition de la Rune + Fusion de la Rune
+        runeParticle.SetActive(true);
+        yield return new WaitForSeconds(4.5f);
+        runeUniteAnimator.SetBool("Reunite", true);
+        runeParticle.SetActive(false);
+        //Insérer le Flash
+        Debug.Log("Réuni");
+        yield return new WaitForSeconds(4.5f);
+
+        SceneManager.LoadScene(0);
+
+    }
+
+
+}
---
+++ Assets/TRX_Scripts/CinematicScripts/PickupRune.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PickupRune : MonoBehaviour
+{
+    private BoxCollider2D triggerCollider;
+    private Collider2D playerCollider;
+    private Animator runeAnimator;
+    private Animator playerAnimator;
+    private Rigidbody2D playerRigidbody;
+    public GameObject runeParticle;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggerCollider = gameObject.GetComponent<BoxCollider2D>();
+        playerCollider = GameObject.FindWithTag("Player").GetComponent<Collider2D>();
+        playerRigidbody = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+        runeAnimator = GameObject.Find("Rune").GetComponent<Animator>();
+        playerAnimator = GameObject.FindWithTag("Player").GetComponent<Animator>();
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D playerCollider)
+    {
+        StartCoroutine("Timeline");
+        Debug.Log("Timeline lancée !");
+    }
+
+    IEnumerator Timeline()
+    {
+
+        //Lancer l'animation de Twitch de la Rune
+        runeAnimator.SetBool("Pickup", true);
+        runeParticle.SetActive(true);
+        //Freeze le player
+        playerRigidbody.constraints = RigidbodyConstraints2D.FreezePosition;
+        yield return new WaitForSeconds(3.5f);
+
+        //Enlève le ParticleSystem + Animation de Disparition
+        runeParticle.SetActive(false);
+        runeAnimator.SetBool("Disparition", true);
+        yield return new WaitForSeconds(2.5f);
+
+        //Lancer l'anim de Disparition du Golem
+        playerAnimator.SetBool("Disappear", true);
+
+        //Flash + Changement de scène (arrivée dans le temple final)
+        SceneManager.LoadScene(9);
+
+
+    }
+}
